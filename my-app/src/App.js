eb3991c6d3fc06a2000868314d752d3d0ade247a
JonasWesselius <112404721+JonasWesselius@users.noreply.github.com>
2025-05-27 17:38:52 +0200

    Init CV Creator
    -created the cv page
    -created a createcv page
    -made a simple navbar
    -made a service folder

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,25 +1,26 @@
 import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Home from "./pages/home";
+import CV from "./pages/cv";
+import CreateCV from "./pages/createCV";
+import Jobs from "./pages/jobs";
+import Guide from "./pages/guide";
+import Tests from "./pages/tests";
+import Navbar from "./components/navbar";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/cv" element={<CV />} />
+          <Route path="/create-cv" element={<CreateCV />} />
+          <Route path="/jobs" element={<Jobs />} />
+          <Route path="/guide" element={<Guide />} />
+          <Route path="/tests" element={<Tests />} />
+        </Routes>
+        <Navbar />
+    </Router>
   );
 }
 
--- my-app/src/pages/guide.js
+++ my-app/src/pages/guide.js
@@ -1,1 +1,12 @@
-test
+import React from 'react';
+
+function Guide() {
+  return (
+    <div className="guide-page">
+      <h1>Guide</h1>
+      <p>Welcome to the Guide page.</p>
+    </div>
+  );
+}
+
+export default Guide;
