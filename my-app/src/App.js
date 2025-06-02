db5141b515bf865a82d1a9f4027cd87513b6af1b
Luca <lucastefan.tomescu@gmail.com>
2025-06-02 15:56:43 +0200

    +login/signup start rs123

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -8,6 +8,8 @@ import Jobs from "./pages/jobs";
 import Guide from "./pages/guide";
 import Tests from "./pages/tests";
 import Navbar from "./components/navbar";
+import Login from "./pages/login";
+import Signup from "./pages/signup";
 
 
 function App() {
@@ -20,6 +22,8 @@ function App() {
       <Route path="/jobs" element={<Jobs />} />
       <Route path="/guide" element={<Guide />} />
       <Route path="/tests" element={<Tests />} />
+      <Route path="/login" element={<Login />} />
+      <Route path="/signup" element={<Signup />} />
     </Routes>
     <Navbar />
 </Router>
--- my-app/src/pages/home.js
+++ my-app/src/pages/home.js
@@ -1,10 +1,13 @@
 import React from 'react';
+import AuthButtons from '../components/AuthButtons';
+import './home.css';
 
 function Home() {
   return (
     <div className="home-page">
       <h1>Welcome to ReFuture</h1>
       <p>Your career development platform.</p>
+      <AuthButtons />
     </div>
   );
 }
